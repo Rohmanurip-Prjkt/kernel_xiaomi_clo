09c4ba1abb1c1aa7aadfa8af4671cba868766b16
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-02-03 21:44:07 +0700

    Create nextpatch.sh

---
+++ nextpatch.sh
@@ -0,0 +1,4 @@
+rm -rf KernelSU
+curl -LSs "https://raw.githubusercontent.com/Impqxr/KernelSU-Next/refs/heads/legacy-susfs/kernel/setup.sh" | bash -s legacy-susfs
+echo "Adding more manager support..."
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
